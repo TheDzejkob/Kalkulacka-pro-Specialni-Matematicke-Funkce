8ef3b2589c0be94d3a056ae83fce5f20f594429e
TheDzejkob <125360256+TheDzejkob@users.noreply.github.com>
2024-01-24 18:25:13 +0000

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function TOOL(){
-    let vysledeLabel = document.getElementById("vysl");
+    let vysledeLabel = document.getElementById("vysl-fibo");
     let veli = document.getElementById("velikost");
     var i = veli.value;
     var cis1 = 0;
@@ -15,8 +15,9 @@ function TOOL(){
     }
     vysledeLabel.textContent = list.join(', ');
 }
+
 function logari(){
-    let vysledeLabel = document.getElementById("vysl");
+    let vysledeLabel = document.getElementById("vysl-loga");
     var base = document.getElementById("base").value;
     var hodnota = document.getElementById("hodnota").value;
 
@@ -25,15 +26,33 @@ function logari(){
 }
 
 function cosi(){
-    let vysledeLabel = document.getElementById("vysl");
+    let vysledeLabel = document.getElementById("vysl-cosi");
     var uhel = document.getElementById("uhel").value;
 
     const vys = Math.cos(uhel);
     vysledeLabel.textContent = vys;
 }
+
 function sini(){
-    let vysledeLabel = document.getElementById("vysl");
+    let vysledeLabel = document.getElementById("vysl-sini");
+    var uhel = document.getElementById("uhel1").value;
+    const vys = Math.sin(uhel * Math.PI / 180);
+    vysledeLabel.textContent = vys;
 }
+
+function faki(){
+    let vysledeLabel = document.getElementById("vysl-faki");
+    var hodnotaFaktorial = document.getElementById("hod").value;
+        const faktorialResult = (function factorial(n) {
+            if (n === 0 || n === 1) {
+                    return 1;
+            } else {
+                return n * factorial(n - 1);
+            }
+        })(parseInt(hodnotaFaktorial));
+    vysledeLabel.textContent = faktorialResult;
+}
+
 
 function sel(){
     var a = document.getElementById("select").value;
@@ -42,18 +61,50 @@ function sel(){
         document.getElementById("fibo").style.display="block"
         document.getElementById("loga").style.display="none"
         document.getElementById("cosi").style.display="none"
+        document.getElementById("sini").style.display="none"
+        document.getElementById("faki").style.display="none"
         vysledeLabel.textContent=""
     }
     if(a== "LOG"){
         document.getElementById("loga").style.display="block"
         document.getElementById("fibo").style.display="none"
         document.getElementById("cosi").style.display="none"
+        document.getElementById("sini").style.display="none"
+        document.getElementById("faki").style.display="none"
         vysledeLabel.textContent=""
     }
     if(a== "COS"){
         document.getElementById("cosi").style.display="block"
         document.getElementById("loga").style.display="none"
         document.getElementById("fibo").style.display="none"
+        document.getElementById("sini").style.display="none"
+        document.getElementById("faki").style.display="none"
+        vysledeLabel.textContent=""
+    }
+    if(a== "SIN"){
+        document.getElementById("sini").style.display="block"
+        document.getElementById("loga").style.display="none"
+        document.getElementById("fibo").style.display="none"
+        document.getElementById("cosi").style.display="none"
+        document.getElementById("faki").style.display="none"
+        vysledeLabel.textContent=""
+    }
+    if(a== "FAK"){
+        document.getElementById("faki").style.display="block"
+        document.getElementById("sini").style.display="none"
+        document.getElementById("loga").style.display="none"
+        document.getElementById("fibo").style.display="none"
+        document.getElementById("cosi").style.display="none"
+        vysledeLabel.textContent=""
+    }
+
+
+    if(a== "default"){
+        document.getElementById("loga").style.display="none"
+        document.getElementById("fibo").style.display="none"
+        document.getElementById("cosi").style.display="none"
+        document.getElementById("sini").style.display="none"
+        document.getElementById("faki").style.display="none"
         vysledeLabel.textContent=""
     }
     else{
